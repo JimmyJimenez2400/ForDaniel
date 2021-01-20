6fab560ac355c5f8029e8f45b29e4c7b6d1da4f8
JimmyJimenez2400 <jimmy.jimenez@jjay.cuny.edu>
2021-01-20 18:12:09 -0500

    Changed API location
    changed it to coachella

--- src/components/location/index.js
+++ src/components/location/index.js
@@ -5,8 +5,9 @@ const Location = () => {
         <div className="location_wrapper">
             
             <iframe 
-
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.8987292358743!2d-73.93605708433661!3d40.74225384370057!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25ed36e8c0eb1%3A0xf912aae6332e504a!2sIndoor%20Extreme%20Sports%20Paintball%20%26%20Laser%20Tag!5e0!3m2!1sen!2sus!4v1611111440412!5m2!1sen!2sus" 
+                    
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d53118.10551616319!2d-116.18242531317163!3d33.686129980626205!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80da588e87ce3caf%3A0xfd521c797b97bcf8!2sCoachella%2C%20CA!5e0!3m2!1sen!2sus!4v1611184158741!5m2!1sen!2sus"
+                    
                 
 
                 width="100%" 
