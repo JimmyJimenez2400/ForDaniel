8561d3571776949e28ae95f5fae418d794a031aa
JimmyJimenez2400 <jimmy.jimenez@jjay.cuny.edu>
2021-01-21 01:23:36 -0500

    Changed opening day!

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -52,7 +52,7 @@ class Header extends Component {
 
                     <div className="header_logo">
                         <div className="font_righteous header_logo_venue">Arcades Adventures!</div>
-                        <div className="header_logo_title">Opening Day</div>
+                        <div className="header_logo_title">Opening Day!</div>
                     </div>
 
                     <IconButton
